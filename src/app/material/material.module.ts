4b5a996bc4d447bd6bf0e2b5bb9cdeffa31d5f6d
john medel mirafuentes <j0hnm3d3l@gmail.com>
2017-11-06 21:02:52 +0800

    set up material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+      MatButtonModule,
+      MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
